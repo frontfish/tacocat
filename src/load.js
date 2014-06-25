df1865f6d2e6bfcec7eabce39101e811999402da
Christopher Hinstorff <chris@chinstorff.com>
2014-06-24 17:20:23 -0700

    return to boilerplate

--- src/load.js
+++ src/load.js
@@ -3,10 +3,10 @@ Game.Load = function (game) { };
 Game.Load.prototype = {
     preload: function () {
 	// create loading screen
-	game.stage.backgroundColor = "#eee";
+
 
 	// load everything
-	game.load.image('player', 'assets/img/player.png');
+
 
     },
 
